f1bc6dfdef450cf93ef5a48a1c127befb292e112
manish kumar <manishcenter4@gmail.com>
2026-02-22 21:39:00 +0530

    login system added

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const path = require("path");
+const Rendering_router = require("./routers/Rendering_router/rendering_router");
 const port = require("dotenv");
 const app = express();
 
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+
 //all middleware here
+
 app.use(express.json());
-app.use(express.static(path.join("public",__dirname)));
+app.use(express.static(path.join(__dirname,"public")));
+// app.use(express.static(path.join(__dirname,"routers")));
 app.use(express.urlencoded({extended:true}));
 
 
+app.use("/",Rendering_router);
 
-app.get("/",(req,res)=>{
-    res.render("home");
-})
+
 
 
 port.config();
