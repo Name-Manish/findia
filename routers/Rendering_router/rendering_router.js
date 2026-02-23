619328ab06fd097b5f8cb0254ff95cdd3273504b
manish kumar <manishcenter4@gmail.com>
2026-02-23 09:38:46 +0530

    new updated

--- routers/Rendering_router/rendering_router.js
+++ routers/Rendering_router/rendering_router.js
@@ -7,6 +7,15 @@ router.get("/",(req,res)=>{
 router.get("/home",(req,res)=>{
     res.render("home");
 })
+router.get("/categories",(req,res)=>{
+    res.render("categories");
+})
+router.get("/practice",(req,res)=>{
+    res.render("practice");
+})
+router.get("/create",(req,res)=>{
+    res.render("create");
+})
 router.get("/login",(req,res)=>{
     res.render("login");
 })
